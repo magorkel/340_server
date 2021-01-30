71d41304dcf2c3c99eff3692d12fd241ed1f0bf3
DgP958nxNPVq <magorkel@gmail.com>
2021-01-30 14:01:02 -0700

    first commit

---
+++ app/src/main/java/edu/byu/cs/tweeter/model/domain/AuthToken.java
@@ -0,0 +1,10 @@
+package edu.byu.cs.tweeter.model.domain;
+
+import java.io.Serializable;
+
+/**
+ * Represents an auth token in the system.
+ */
+public class AuthToken implements Serializable {
+    //don't need to implement until milestone 4
+}
